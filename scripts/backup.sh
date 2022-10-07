2f1d6d1d4dcc246ec42c4b70e5d87cd2f4cb5314
rahul-trs <96952112+rahul-trs@users.noreply.github.com>
2022-10-07 23:38:38 +0530

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,2 +1,2 @@
-sudo rm -r /home/ubuntu/sample_FE/lastbuild
-mv /home/ubuntu/sample_FE/build /home/ubuntu/sample_FE/lastbuild
+sudo rm -r /home/ubuntu/server/lastbuild
+mv /home/ubuntu/server/build /home/ubuntu/server/lastbuild
