55353b2816f5c8404ce41ec0cd8aebf58708cf81
rahul-trs <96952112+rahul-trs@users.noreply.github.com>
2022-10-09 22:50:45 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,7 @@ cd /home/ubuntu/server
 sudo rm -rf node_modules/
 sudo rm -rf package-lock.json
 sudo apt-get install -y npm
-sudo npm install --save react react-dom react-scripts react-particles-js
+#sudo npm install --save react react-dom react-scripts react-particles-js
+npm install --save core-js@^3
 sudo npm install -g serve
 sudo npm install pm2 -g
